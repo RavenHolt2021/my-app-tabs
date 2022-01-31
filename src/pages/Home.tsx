3f6b82e6ab5d032c0ba8473d1ab769eac2b19287
RavenHolt2021 <81608337+RavenHolt2021@users.noreply.github.com>
2022-01-31 10:42:49 -0800

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,71 @@
+import MessageListItem from '../components/MessageListItem';
+import { useState } from 'react';
+import { Message, getMessages } from '../data/messages';
+import {
+  IonContent,
+  IonHeader,
+  IonList,
+  IonPage,
+  IonRefresher,
+  IonRefresherContent,
+  IonTitle,
+  IonFooter,
+  IonButton,
+  IonToolbar,
+  useIonViewWillEnter
+} from '@ionic/react';
+import './Home.css';
+
+const Home: React.FC = () => {
+
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  useIonViewWillEnter(() => {
+    const msgs = getMessages();
+    setMessages(msgs);
+  });
+
+  const refresh = (e: CustomEvent) => {
+    setTimeout(() => {
+      e.detail.complete();
+    }, 3000);
+  };
+
+  return (
+    <IonPage id="home-page">
+      {/* <IonHeader>
+        <IonToolbar>
+          <IonTitle>Lucid Companion</IonTitle>
+        </IonToolbar>
+      </IonHeader> */}
+
+      {/* Keep this how it is for now. Speak with teacher if need be! */}
+
+      <IonContent color="success" fullscreen>
+        <IonRefresher slot="fixed" onIonRefresh={refresh}>
+          <IonRefresherContent></IonRefresherContent>
+        </IonRefresher>
+
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large" color="danger">
+            Lucid Companion
+            </IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonButton href="#" expand="full" fill="outline" color="dark">Journal</IonButton>
+        <IonButton href="#" expand="full" fill="outline" color="dark">Alarm</IonButton>
+        <IonButton href="#" expand="full" fill="outline" color="dark">Get Lucid</IonButton>
+        <IonButton href="#" expand="full" fill="outline" color="dark">Settings</IonButton>
+        {/* <IonList>
+          {messages.map(m => <MessageListItem key={m.id} message={m} />)}
+        </IonList> */}
+        <IonFooter>
+          The buttons are supposed to be bigger. -_-
+        </IonFooter>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
