24e4b65bb21f354c4e0818ab6eac64e25740d3f7
PerfectFruits <90435081+PerfectFruits@users.noreply.github.com>
2022-02-04 11:10:13 -0800

    Connections and pages for the home menu buttons.

---
+++ SettingsMenu.tsx
@@ -0,0 +1,73 @@
+import MessageListItem from '../components/MessageListItem';
+import { useState } from 'react';
+import { Message, getMessages } from '../data/messages';
+import {
+  IonContent,
+  IonHeader,
+  IonList,
+  IonPage,
+  IonRefresher,
+  IonRefresherContent,
+  IonTitle,
+  IonFooter,
+  IonButton,
+  IonToolbar,
+  useIonViewWillEnter,
+  IonBackButton
+} from '@ionic/react';
+import './Home.css';
+
+const SettingsMenu: React.FC = () => {
+
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  useIonViewWillEnter(() => {
+    const msgs = getMessages();
+    setMessages(msgs);
+  });
+
+  const refresh = (e: CustomEvent) => {
+    setTimeout(() => {
+      e.detail.complete();
+    }, 3000);
+  };
+
+  return (
+    <IonPage id="home-page">
+      {/* <IonHeader>
+        <IonToolbar>
+          <IonTitle>Lucid Companion</IonTitle>
+        </IonToolbar>
+      </IonHeader> */}
+
+      {/* Keep this how it is for now. Speak with teacher if need be! */}
+
+      <IonContent color="success" fullscreen>
+        <IonRefresher slot="fixed" onIonRefresh={refresh}>
+          <IonRefresherContent></IonRefresherContent>
+        </IonRefresher>
+
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonButton slot="start">
+              <IonBackButton></IonBackButton>
+            </IonButton>
+            <IonTitle className="title-bar">
+            Settings
+            </IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonButton href="#" className="big-button" expand="full" fill="outline" color="dark">Screen Settings</IonButton>
+        <IonButton href="#" className="big-button" expand="full" fill="outline" color="dark">Data Settings</IonButton>
+        <IonButton href="#" className="big-button" expand="full" fill="outline" color="dark">Passwords</IonButton>
+        {/* <IonList>
+          {messages.map(m => <MessageListItem key={m.id} message={m} />)}
+        </IonList> */}
+        <IonFooter className="footer-content">
+        </IonFooter>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default SettingsMenu;
